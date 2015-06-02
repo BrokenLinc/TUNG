9414d96d927e73f31e49aadf60c4435c6a3a6fc5
Lincoln Anderson <landerson@352inc.com>
2015-06-02 00:13:42 -0400

    Adding usemin.

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,37 @@ var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 var less = require('gulp-less');
 var livereload = require('gulp-livereload');
+var minifyCss = require('gulp-minify-css');
 var plumber = require('gulp-plumber');
+var uglify = require('gulp-uglify');
+var usemin = require('gulp-usemin');
 var watch = require('gulp-watch');
  
 gulp.task('less', function () {
-	return gulp.src('./dev/less/*.less')
+	return gulp.src('./src/less/*.less')
 		.pipe(plumber())
 		.pipe(less())
 		.pipe(autoprefixer("last 1 version", "> 0.5%"))
-		.pipe(gulp.dest('./dev/css'));
+		.pipe(gulp.dest('./src/css'));
 });
  
 gulp.task('watch', function () {
-	gulp.watch('./dev/less/**/*.less', ['less']);
+	gulp.watch('./src/less/**/*.less', ['less']);
  
 	//refresh only files that change
-	return gulp.src('./dev/css/**/*.css')
-		.pipe(watch('./dev/css/**/*.css'))
+	return gulp.src('./src/css/**/*.css')
+		.pipe(watch('./src/css/**/*.css'))
 		.pipe(livereload({ start: true }));
 });
- 
+
+gulp.task('usemin', function () {
+	return gulp.src('./src/index.html')
+		.pipe(usemin({
+	        inlinejs: [uglify()],
+	        inlinecss: [minifyCss(), 'concat']
+		}))
+		.pipe(gulp.dest('dist/'));
+});
+
+gulp.task('dist', ['less', 'usemin']);
 gulp.task('default', ['less', 'watch']);
